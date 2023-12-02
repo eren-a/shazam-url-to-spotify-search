5fb3afee169f11b06ef5343c80c8bb356642d996
Eren Akdeniz <erenakdeniz0223@gmail.com>
2023-12-02 17:40:48 +0100

    updated timeout

--- content.js
+++ content.js
@@ -1,4 +1,7 @@
 setTimeout(() => {
     const trackName = window.location.pathname.split('/').pop();
     chrome.runtime.sendMessage({ action: "openSpotify", trackName });
-}, 500);
+}, 1000);
+
+// 1000ms: url might be incorrect, due to redirection
+// longer timeout prevents that
